e00797d3a3a804b469c8a77967e23e996da8a6c9
kyanscienceman <kevin.yan@upandup.co>
2025-07-31 17:30:04 -0400

    leasing funnel

--- dashboards/apps/leasing/app.py
+++ dashboards/apps/leasing/app.py
@@ -4,7 +4,7 @@ from google.oauth2 import service_account
 
 from data import get_service_account_info, leasing_scraper_data, conversion_rates_data
 from tabs.competitors_tab import metrics, competitors_filters, clearance_rates, rent_changes, turn_times
-from tabs.conversion_rates_tab import conversion_rates
+from tabs.conversion_rates_tab import conversion_rates_filters, conversion_rates, leasing_funnel_defs
 
 # Configure page layout
 st.set_page_config(
@@ -23,7 +23,7 @@ conversion_rates_df = conversion_rates_data(credentials)
 # Application
 st.title("Leasing Dashboard")
 
-competitors_tab, conversion_rates_tab = st.tabs(["Competitors", 'Conversion Rates'])
+competitors_tab, conversion_rates_tab = st.tabs(["Competitors", 'Conversion Rates (WIP)'])
 with competitors_tab:
     leasing_period_df, start_date, end_date, color_scale = competitors_filters(leasing_df)
     metrics()
@@ -32,7 +32,9 @@ with competitors_tab:
     turn_times(leasing_period_df, color_scale)
 with conversion_rates_tab:
     # KEVIN TO DO
-    conversion_rates(conversion_rates_df)
+    filtered_conversion_rates_df = conversion_rates_filters(conversion_rates_df)
+    leasing_funnel_defs()
+    conversion_rates(filtered_conversion_rates_df)
 
 
 
