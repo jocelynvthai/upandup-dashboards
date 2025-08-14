cf3652a8c5b657e1898ab9393eda497c9d60bcc5
kyanscienceman <kevin.yan@upandup.co>
2025-08-14 13:36:29 -0400

    add metrics

--- dashboards/apps/turns/tabs/individual_turn_drilldown.py
+++ dashboards/apps/turns/tabs/individual_turn_drilldown.py
@@ -1,6 +1,8 @@
+import pandas as pd
 import streamlit as st
 
 DELIMITER = ' -- moved out '
+NULL_VALUE = "N/A"
 
 def drilldown_filters(turns_df, line_items_df):
     distinct_turns = turns_df['address'] + DELIMITER + turns_df['move_out_date'].astype(str)
@@ -19,19 +21,54 @@ def drilldown_filters(turns_df, line_items_df):
 
 
 def individual_turn_summary(filtered_line_items_df, filtered_turns_df):
-    col1, col2, col3, col4 = st.columns(4)
+    col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
     turn = filtered_turns_df.iloc[0]
     with col1:
         st.metric("Billed Cost", f"${filtered_line_items_df['amount'].sum():,.2f}")
     with col2:
-        estimated_cost = turn['project_estimated_cost']
+        project_estimated_cost = turn['project_estimated_cost'] if not pd.isnull(turn['project_estimated_cost']) else 0
+        ticket_approved_budgets = turn['project_ticket_approved_budgets'] if not pd.isnull(turn['project_ticket_approved_budgets']) else 0
+        estimated_cost = project_estimated_cost
         if turn['fund'] != 'Homevest Real Estate Partners IV - Limestone, L.P.':
-            estimated_cost += turn['project_ticket_approved_budgets']
+            estimated_cost += ticket_approved_budgets
         st.metric("Estimated Cost", f"${estimated_cost:,.2f}")
     with col3:
         st.metric("Move-Out Date", f"{turn['move_out_date']:%m/%d/%Y}")
     with col4:
-        st.metric("Scoped Date", f"{turn['project_scoped_at']:%m/%d/%Y}")
+        col4_title = "Scoped Date"
+        project_scoped_date = turn['project_scoped_date']
+        if not pd.isnull(project_scoped_date):
+          st.metric(col4_title, f"{project_scoped_date:%m/%d/%Y}")
+        else:
+          st.metric(col4_title, NULL_VALUE)
+    with col5:
+        col5_title = "Scope Approved Date"
+        project_scope_approved_date = turn['project_scope_approved_date']
+        if not pd.isnull(project_scoped_date):
+          st.metric(col5_title, f"{project_scope_approved_date:%m/%d/%Y}")
+        else:
+          st.metric(col5_title, NULL_VALUE)
+    with col6:
+        col6_title = "Finished QC Date"
+        project_finished_qc_date = turn['project_finished_qc_date']
+        if not pd.isnull(project_finished_qc_date):
+          st.metric(col6_title, f"{project_finished_qc_date:%m/%d/%Y}")
+        else:
+          st.metric(col6_title, NULL_VALUE)
+    with col7:
+        col7_title = "Tour Ready Date"
+        tour_ready_date = turn['tour_ready_date']
+        if not pd.isnull(tour_ready_date):
+          st.metric(col7_title, f"{tour_ready_date:%m/%d/%Y}")
+        else:
+          st.metric(col7_title, NULL_VALUE)
+    with col8:
+        col8_title = "Rent Ready Date"
+        rent_ready_date = turn['rent_ready_date']
+        if not pd.isnull(rent_ready_date):
+          st.metric(col8_title, f"{rent_ready_date:%m/%d/%Y}")
+        else:
+          st.metric(col8_title, NULL_VALUE)
 
 
 def individual_turn_drilldown(filtered_line_items_df):
