343e6102ff5ac9f4dbb70a6b11799d98b74ce586
Jocelyn Thai <jocelyn.thai@upandup.co>
2025-08-18 16:47:54 -0400

    local fix

--- dashboards/apps/leasing/app.py
+++ dashboards/apps/leasing/app.py
@@ -20,7 +20,7 @@ st.set_page_config(
 )
 
 # Data Retrieval
-credentials = service_account.Credentials.from_service_account_info(get_service_account_info(local=True))
+credentials = service_account.Credentials.from_service_account_info(get_service_account_info())
 leasing_df = leasing_scraper_data(credentials)
 rental_applications_df = rental_applications_data(credentials)
 leasing_funnel_df = leasing_funnel_data(credentials)
