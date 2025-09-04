756b90436b2c6cad32d64a92bf2f437c11b7a6b8
Jocelyn Thai <jocelyn.thai@upandup.co>
2025-09-04 17:49:40 -0400

    num leases needed to target

--- dashboards/apps/leasing/tabs/occupancy_tab.py
+++ dashboards/apps/leasing/tabs/occupancy_tab.py
@@ -7,6 +7,8 @@ from dateutil.relativedelta import relativedelta
 from tabs.utils import GRAY, TEAL, PURPLE
 
 
+TODAY = datetime.now().date()
+
 def occupancy_filters(economic_occupancy_df, rental_df):
     col_fund, col_market = st.columns(2)
 
@@ -45,28 +47,26 @@ def economic_occupancy(economic_occupancy_df):
     with time_granularity_col:
         selected_time_granularity = st.selectbox("Select a time granularity", ['day', 'week', 'month'], index=1)
     with date_range_col:
-        today = datetime.now().date()
         last_date = economic_occupancy_df['date'].max()
         if selected_time_granularity == 'day':
-            start = today
-            selected_days = st.slider("Select # days to view", min_value=1, max_value=(last_date-today).days, value=60)
-            end = today + relativedelta(days=selected_days)
+            start = TODAY
+            selected_days = st.slider("Select # days to view", min_value=1, max_value=(last_date-TODAY).days, value=60)
+            end = TODAY + relativedelta(days=selected_days)
         elif selected_time_granularity == 'week':
-            start = today - relativedelta(days=datetime.now().weekday())
+            start = TODAY - relativedelta(days=datetime.now().weekday())
             last_week_start = last_date - relativedelta(days=last_date.weekday())
             selected_weeks = st.slider("Select # weeks to view", min_value=1, max_value=int((last_week_start-start).days/7), value=8)
             end = start + relativedelta(weeks=selected_weeks)
         else:
-            start = today - relativedelta(days=today.day-1)
+            start = TODAY - relativedelta(days=TODAY.day-1)
             last_month_start = last_date - relativedelta(days=last_date.day-1)
             selected_months = st.slider("Select # months to view", min_value=1, max_value=int((last_month_start-start).days/30), value=3)
             end = start + relativedelta(months=selected_months)
 
-
-
     economic_occupancy_selected = economic_occupancy_df[(economic_occupancy_df['time_granularity'] == selected_time_granularity) &
                                                         (economic_occupancy_df['date'] >= start) &
-                                                        (economic_occupancy_df['date'] < end)].groupby('date').agg(
+                                                        (economic_occupancy_df['date'] < end)
+    ].groupby('date').agg(
         total_gpr=('total_gpr', 'sum'), 
         total_gpr_potentially_occupied=('total_gpr_potentially_occupied', 'sum'),
         total_gpr_occupied=('total_gpr_occupied', 'sum'), 
@@ -108,7 +108,36 @@ def economic_occupancy(economic_occupancy_df):
 
 def num_leases_to_target(economic_occupancy_df):
     st.subheader("Number of Leases to Target")
-    day_economic_occupancy = economic_occupancy_df[economic_occupancy_df['time_granularity'] == 'day'].groupby('date').agg(
+    deadline_col, deadline_date_col = st.columns([6, 1])
+    with deadline_col:
+        deadline_options = {
+            'This week': TODAY + relativedelta(weeks=0, weekday=6),
+            'Next week': TODAY + relativedelta(weeks=1, weekday=6), 
+            'Next next week': TODAY + relativedelta(weeks=2, weekday=6), 
+            'This month': TODAY.replace(day=1) + relativedelta(months=1) - relativedelta(days=1), 
+            'Next month': TODAY.replace(day=1) + relativedelta(months=2) - relativedelta(days=1)
+        }
+        # deadline_options_display = [f"{key} ({value})" for key, value in deadline_options.items()]
+        selected_deadline = st.selectbox("Select a deadline", list(deadline_options.keys()), 
+                                        index=0, 
+                                        help="Deadline date is end of the selected time period",
+                                        key="num_leases_to_target_deadline")
+    with deadline_date_col:
+        deadline_date = deadline_options[selected_deadline]
+        st.markdown(
+            f"""
+            <div style='text-align: right; padding: 10px'>
+                <h6>Deadline Date</h6>
+                <h6 style='color: {TEAL};'>{deadline_date.strftime('%Y-%m-%d')}</h6>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+
+    day_economic_occupancy = economic_occupancy_df[(economic_occupancy_df['time_granularity'] == 'day') &
+                                                    (economic_occupancy_df['date'] >= TODAY) &
+                                                    (economic_occupancy_df['date'] <= deadline_date)
+    ].groupby('date').agg(
         num_properties=('num_properties', 'sum'),
         num_properties_potentially_occupied=('num_properties_potentially_occupied', 'sum'),
         num_properties_occupied=('num_properties_occupied', 'sum'),
@@ -117,8 +146,26 @@ def num_leases_to_target(economic_occupancy_df):
         total_gpr_occupied=('total_gpr_occupied', 'sum'),
         total_gpr_occupied_budget=('total_gpr_occupied_budget', 'sum')
     ).reset_index()
+
     day_economic_occupancy['hole'] = day_economic_occupancy['total_gpr_occupied_budget'] - day_economic_occupancy['total_gpr_occupied']
+    deadline_row = day_economic_occupancy[day_economic_occupancy['date'] == deadline_date]
+    hole_col, vacant_homes_col, recoverable_gpr_col, leases_needed_col = st.columns(4)
+    with hole_col:
+        hole = deadline_row['hole'].iloc[0]
+        st.metric("Hole", f"{hole:,.2f}")
+    with vacant_homes_col:
+        vacant_homes = deadline_row['num_properties'].iloc[0] - deadline_row['num_properties_occupied'].iloc[0]
+        st.metric("Vacant Homes", f"{vacant_homes:,.2f}")
+    with recoverable_gpr_col:
+        recoverable_gpr_per_home_per_day = (deadline_row['total_gpr'].iloc[0] - deadline_row['total_gpr_occupied'].iloc[0]) / (deadline_row['num_properties'].iloc[0] - deadline_row['num_properties_occupied'].iloc[0])
+        st.metric("Recoverable GPR per Home per Day", f"{recoverable_gpr_per_home_per_day:,.2f}")
+    with leases_needed_col:
+        leases_needed = hole / recoverable_gpr_per_home_per_day
+        st.metric("Leases Needed", f"{leases_needed:,.0f}")
+
+    st.write('**Recovery Days Table:**')
     st.dataframe(day_economic_occupancy)
+
 
 
 def upcoming_moves(rental_df): 
@@ -174,13 +221,5 @@ def upcoming_moves(rental_df):
                             <tbody>{"".join(html_rows)}</tbody>
                         </table>'''
         st.markdown(html_table, unsafe_allow_html=True)
-
-
-
-
-
-
-
-
 
 
